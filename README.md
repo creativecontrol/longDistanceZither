00a062763826e5412f08e653f09bfc4ba3296bd6
Thadeus Reed <t@creativecontrol.cc>
2020-11-17 23:45:24 -0800

    First functioning verision. MIDI input gets translated to 0-1 messages and stored in Realtime Database per string. Next Step: Retrieve data when it changes and send MIDI out.

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 Long Distance Zither consists of two physical keyed zithers whose strings are activated at both ends.
 These zithers can be connected to a computer and to each other over the internet so that
-the notes played one zither are transmitted the other.
+the notes played on one zither are transmitted the other.
 
 This connection is facilitated through a client/server relationship similar to a chat application.
 ToneJS and Google Chrome allow for a USB MIDI connection from the embedded electronics in the zither
--- public/js/longDistance.js
+++ public/js/longDistance.js
@@ -10,24 +10,77 @@ class LongDistance {
     this.currentInput = null;
     this.curretOutput = null;
 
-    this.setListeners();
+    this.pitchMap = [60,62,64,65,67,69,71,72,74,76,77,79,81];
+
+    this.instrumentSelectAction = document.getElementById('whichZither');
+    this.ON = 1;
+    this.OFF = 0;
+
+    this.database;
+    this.instrument = document.getElementById('whichZither').value;
+
+    firebase.auth().signInAnonymously()
+      .then(() => {
+        this.connectToDatabase();
+        this.setListeners();
+      })
+      .catch((error) => {
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        // ...
+      })
   }
 
   setListeners () {
     let that = this;
 
-    this.activateMidiAction.onclick = ()=> {
+    this.activateMidiAction.onclick = () => {
       that.midiInitButton.style.visibility = "hidden";
       that.midiUI.style.visibility = "visible";
       Tone.context.resume();
       that.initMidi();
     }
 
-    this.inputMenu.onchange = (midi)=> {
-        that.currentInput = WebMidi.getInputById(midi.id);
+    // Think about making this an initial login state or based on URL to avoid unknown states
+    this.instrumentSelectAction.onchange = () => {
+      this.instrument = document.getElementById('whichZither').value;
+      console.log(this.instrument + ' selected');
+    }
+
+    this.inputMenu.onchange = () => {
+        if (that.currentInput) {
+          that.currentInput.removeListener();
+        }
+        try {
+          that.currentInput = WebMidi.getInputById(that.inputMenu.value);
+          console.log(that.currentInput);
+
+          that.currentInput.addListener('noteon', 'all', (midiEvent) => {
+            console.log ("NOTE ON");
+            console.log("midi number: " + midiEvent.note.number);
+            console.log("midi velocity: " + midiEvent.velocity);
+            let key = that.pitchMap.indexOf(midiEvent.note.number);
+            console.log("key: " + key);
+            that.database.ref(this.instrument + '/' + key.toString()).set(that.ON);
+          });
+
+          that.currentInput.addListener('noteoff', 'all', (midiEvent) => {
+            console.log ("NOTE OFF");
+            console.log("midi number: " + midiEvent.note.number);
+            console.log("midi velocity: " + midiEvent.velocity);
+            let key = that.pitchMap.indexOf(midiEvent.note.number);
+            console.log("key: " + key);
+            that.database.ref(this.instrument + '/' + key.toString()).set(that.OFF);
+          });
+
+
+        } catch (error) {
+          console.error(error);
+        }
     };
-    this.outputMenu.onchange = (midi)=> {
-      that.currentOutput = WebMidi.getOutputById(midi.id);
+    this.outputMenu.onchange = ()=> {
+      that.currentOutput = WebMidi.getOutputById(that.outputMenu.value);
+      console.log(that.currentOutput);
     }
 
   }
@@ -37,47 +90,64 @@ class LongDistance {
 
     WebMidi.enable(function (err) {
       if (err) {
-      console.log("WebMidi could not be enabled.", err);
-      return;
-    } else {
-      console.log("WebMidi enabled!");
-    }
+        console.log("WebMidi could not be enabled.", err);
+        return;
+      } else {
+        console.log("WebMidi enabled!");
+      }
 
-    // Reacting when a new device becomes available
-    WebMidi.addListener("connected", function(e) {
-      console.log(e);
+      // Reacting when a new device becomes available
+      // Will need to update to check if input already exists
+      WebMidi.addListener("connected", function(e) {
+        // that.fillInputs();
+        // that.fillOutputs();
+      });
+
+      // Reacting when a device becomes unavailable
+      WebMidi.addListener("disconnected", function(e) {
+        // that.fillInputs();
+        // that.fillOutputs();
+      });
+
+      that.fillInputs();
+      that.fillOutputs();
     });
 
-    // Reacting when a device becomes unavailable
-    WebMidi.addListener("disconnected", function(e) {
-      console.log(e);
-    });
+  }
+
+  fillInputs() {
+    let that = this;
 
     WebMidi.inputs.forEach(input => {
       let option = document.createElement("option");
       option.value = input.id;
       option.textContent = 'MIDI: ' + input.name;
       that.inputMenu.appendChild(option);
-
     });
+
+    // if (that.currentInput) {
+    //   that.inputMenu.value = that.currentInput;
+    // }
+  }
+
+  fillOutputs() {
+    let that = this;
 
     WebMidi.outputs.forEach(output => {
       let option = document.createElement("option");
       option.value = output.id;
       option.textContent = 'MIDI: ' + output.name;
       that.outputMenu.appendChild(option);
-
     });
 
-    });
-
-    // midiInput.addListener('noteon', "all", function(evt) {
-    //   console.log("Pitch value: " + evt.value);
-    //   // scheduleDelay(delay, evt);
-    // });
-
+    // if (that.currentOutput) {
+    //   that.outputMenu.value = that.currentOutput;
+    // }
   }
 
+  connectToDatabase() {
+    this.database = firebase.database();
+  }
 }
 
 window.onload = ()=> {
